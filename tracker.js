c43afb473db30c8b6b19a62dfeeb33d4ac02e604
Gofile <yantonuri917@gmail.com>
2026-07-02 13:20:55 +0700

    Create tracker.js

---
+++ tracker.js
@@ -0,0 +1,33 @@
+var _Hasync=_Hasync||[];
+
+
+
+_Hasync.push(['Histats.start','1,4971849,4,0,0,0,00000000']);
+
+
+
+_Hasync.push(['Histats.fasi','1']);
+
+
+
+_Hasync.push(['Histats.track_hits','']);
+
+
+
+(function(){
+
+
+
+  var hs=document.createElement('script');hs.type='text/javascript';hs.async=true;
+
+
+
+  hs.src=('//s10.histats.com/js15_as.js');
+
+
+
+  (document.head||document.body).appendChild(hs);
+
+
+
+})();
