24e6a063f192bb5529e4e8b280c95497812e54a7
Gofile <yantonuri917@gmail.com>
2026-07-11 19:20:33 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+   
+    SUPABASE_URL: 'https://ehhrvswzhirrnrqxmxae.supabase.co',
+    
+    
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoaHJ2c3d6aGlycm5ycXhteGFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMjg2NDcsImV4cCI6MjA5NTgwNDY0N30.75vZs3Rn6j7YSlInHpGDN39OGjhywcEBv8j-hawyOlY'
+};
